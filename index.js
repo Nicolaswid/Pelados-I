2ae27dc609607e0433c2d11b4d4ca69964329cc7
Nicolaswid <nicolaswidder@gmail.com>
2025-12-03 11:48:52 -0300

    Pelados I

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ import {
   seleccionarProducto,
   seleccionarSabores,
 } from "./dataEntry.js";
+import fs from "fs";
 
 // Cargar sabores y productos
 // COMPLETEN USTEDES
-let sabores;
-let productos;
+let sabores = JSON.parse(fs.readFileSync("data/sabores.json"));
+let productos = JSON.parse(fs.readFileSync("data/productos.json"));
 
 // Ingresar cliente
 let cliente = ingresarCliente();
@@ -18,6 +19,14 @@ let producto = seleccionarProducto(productos);
 let gustos = seleccionarCantidadGustos(producto.maxGustos);
 // Elegir sabores
 let saboresElegidos = seleccionarSabores(sabores, gustos);
+
+let infoPedido = JSON.parse(fs.readFileSync("data/pedidos.json", "utf-8"))
+infoPedido.push(cliente);
+infoPedido.push(producto);
+infoPedido.push(saboresElegidos);
+let nuevoJson = JSON.stringify(setDefaultOptions, null, 2);
+
+fs.writeFileSync("data/pedidos.json", nuevoJson);
 
 // Guardar pedido
 // COMPLETEN USTEDES
